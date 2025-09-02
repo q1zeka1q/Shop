c2caf058341268d2be73aca0675673e60825fb60
EvgeniyVasiliev <q1zeka1q@gmail.com>
2025-09-02 16:29:23 +0300

    Добавьте файлы проекта.

---
+++ shoptar/Controllers/SpaceshipsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace shoptar.Controllers
+{
+    public class SpaceshipsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ shoptar/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace shoptar.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
